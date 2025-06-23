8d69eef370b9a2c887e602e618ad67e8b53206b1
faiyazmahmud75 <faiyazmahmud75@gmail.com>
2025-06-23 23:39:33 +0600

    News links collected

--- scrapers/url_crawler.py
+++ scrapers/url_crawler.py
@@ -3,37 +3,38 @@ from datetime import datetime, timedelta
 from bs4 import BeautifulSoup
 import time
 import random
+import json
 
 # end date for scraping links
-end_date = datetime(2025, 6, 1)
+end_date = datetime(2025, 6, 3)
 
 # Newspaper info
 newspapers = {
-    'Kaler Kantho': {
-        'start_date': datetime(2024, 1, 1),
-        'template': 'https://www.kalerkantho.com/daily-sitemap/{date}/sitemap.xml',
-        'domain': 'https://www.kalerkantho.com'
-    },
-    'BD Pratidin': {
-        'start_date': datetime(2022, 1, 1),
-        'template': 'https://www.bd-pratidin.com/daily-sitemap/{date}/sitemap.xml',
-        'domain': 'https://www.bd-pratidin.com'
-    },
+    # 'Kaler Kantho': {
+    #     'start_date': datetime(2024, 1, 1),
+    #     'template': 'https://www.kalerkantho.com/daily-sitemap/{date}/sitemap.xml',
+    #     'domain': 'https://www.kalerkantho.com'
+    # },
+    # 'BD Pratidin': {
+    #     'start_date': datetime(2023, 1, 1),
+    #     'template': 'https://www.bd-pratidin.com/daily-sitemap/{date}/sitemap.xml',
+    #     'domain': 'https://www.bd-pratidin.com'
+    # },
     'Samakal': {
-        'start_date': datetime(2019, 9, 30),
+        'start_date': datetime(2025, 6, 2),
         'template': 'https://samakal.com/sitemap/sitemap-daily-{date}.xml',
         'domain': 'https://samakal.com'
     },
-    'Prothom Alo': {
-        'start_date': datetime(2009, 8, 28),
-        'template': 'https://www.prothomalo.com/sitemap/sitemap-daily-{date}.xml',
-        'domain': 'https://www.prothomalo.com'
-    },
-    'Bhorer Kagoj': {
-        'start_date': datetime(2017, 10, 3),
-        'template': 'https://bhorerkagoj.com/sitemap/sitemap-daily-{date}.xml',
-        'domain': 'https://bhorerkagoj.com'
-    }
+    # 'Prothom Alo': {
+    #     'start_date': datetime(2009, 8, 28),
+    #     'template': 'https://www.prothomalo.com/sitemap/sitemap-daily-{date}.xml',
+    #     'domain': 'https://www.prothomalo.com'
+    # },
+    # 'Bhorer Kagoj': {
+    #     'start_date': datetime(2017, 10, 3),
+    #     'template': 'https://bhorerkagoj.com/sitemap/sitemap-daily-{date}.xml',
+    #     'domain': 'https://bhorerkagoj.com'
+    # }
 }
 # HTTP headers to mimic browser requests
 headers = {
@@ -56,11 +57,14 @@ for paper, item in newspapers.items():
     template = item['template']
     domain = item['domain']
 
-    print(f"\n Crawling for {paper}")
+    print(f"\n ~~~Crawling from {paper}~~~")
     # while loop until start date to end date to scrape links
     while current_date <= end_date:
+
         date_str = current_date.strftime('%Y-%m-%d')
         sitemap_url = template.format(date=date_str)
+        
+        print(f"Extracted [{len(results)}] links so far from ~{str(paper).upper()}~ | Processing date: <{date_str}>", end='\r')
 
         try:
             response = requests.get(sitemap_url, headers=headers, timeout=30)
@@ -90,11 +94,20 @@ for paper, item in newspapers.items():
             print(f"Request Error: {e} — URL: {sitemap_url}")
 
         current_date += timedelta(days=1)
-        time.sleep(random.uniform(1, 3))
+        time.sleep(random.uniform(0.5, 1.5))
 
-
-# Summary
-print("\n ===Done extracting all valid news links===")
 print(f"Total news link found: {len(results)}")
+
+# saving resluts to a Json file
+try:
+    with open('sm_news_links.json', 'w', encoding='utf-8') as f:
+        json.dump(results, f, ensure_ascii=False, indent=4)
+    print(f"Results successfully saved to news_links.json")
+except Exception as e:
+    print(f"Error saving results to file: {e}")
+
+print("\n ===Execution Complete===")
+
 # for row in results[:5]:  # preview first 10
 #     print(row)
+
