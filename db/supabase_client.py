07f7c1ed2926cdf3bc11fe82998c7cce2ff3c868
faiyazmahmud75 <faiyazmahmud75@gmail.com>
2025-06-30 20:03:08 +0600

    fixed issue db connection issue

--- db/supabase_client.py
+++ db/supabase_client.py
@@ -4,8 +4,8 @@ import os
 
 load_dotenv()
 
-SUPABASE_URL = os.Supabae_Url
-SUPABASE_KEY = os.Supabase_Key
+SUPABASE_URL = os.getenv("Supabase_Url")
+SUPABASE_KEY = os.getenv("Supabase_Key")
 
 def get_supabase_client() -> Client:
     return create_client(SUPABASE_URL, SUPABASE_KEY)
